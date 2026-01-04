a507948309201b9b99997a2d80d8f350b44efd05
48984228328Aninha <Amanda_rodrigues.de.siqueira@hotmail.com>
2026-01-04 12:11:50 -0300

    add Produto and customer

--- src/main/java/com/example/demo/domain/customer/Customer.java
+++ src/main/java/com/example/demo/domain/customer/Customer.java
@@ -1,5 +1,40 @@
 package com.example.demo.domain.customer;
 
-public class Customer {
+import jakarta.persistence.Entity;
 
+@Entity
+public class Customer {
+    private Long id;
+    private String username;
+    private String email;
+
+    public Customer() {
+    }
+
+    public Customer(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/com/example/demo/domain/product/Produto.java
+++ src/main/java/com/example/demo/domain/product/Produto.java
@@ -1,5 +1,77 @@
 package com.example.demo.domain.product;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduto;
+    private String nomeProduto;
+    private Double precoProduto;
+    private Integer quantidadeEstoque;
+    private String descricaoProduto;
+    private String categoriaProduto;
+
+    public Produto() {
+    }
+
+    public Produto(Long idProduto, String nomeProduto, Double precoProduto, Integer quantidadeEstoque,
+            String descricaoProduto, String categoriaProduto) {
+        this.idProduto = idProduto;
+        this.nomeProduto = nomeProduto;
+        this.precoProduto = precoProduto;
+        this.quantidadeEstoque = quantidadeEstoque;
+        this.descricaoProduto = descricaoProduto;
+        this.categoriaProduto = categoriaProduto;
+    }
+
+    public Long getIdProduto() {
+        return idProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public Double getPrecoProduto() {
+        return precoProduto;
+    }
+
+    public void setPrecoProduto(Double precoProduto) {
+        this.precoProduto = precoProduto;
+    }
+
+    public Integer getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public String getDescricaoProduto() {
+        return descricaoProduto;
+    }
+
+    public void setDescricaoProduto(String descricaoProduto) {
+        this.descricaoProduto = descricaoProduto;
+    }
+
+    public String getCategoriaProduto() {
+        return categoriaProduto;
+    }
+
+    public void setCategoriaProduto(String categoriaProduto) {
+        this.categoriaProduto = categoriaProduto;
+    }
 }
